5124b38eaeccb5b9f7650eafaa79ee2ad6accd73
Mehedi Hasan <73927315+experthtml@users.noreply.github.com>
2020-11-06 09:51:33 +0600

    html files

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function(){
+
+
+$('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+   return false;
+  });
+
+
+
+// Smoothscroll ///    
+var scroll = new SmoothScroll('.navbar li.nav-item a');
+
+
+// loader
+setTimeout(function(){ 
+
+$('.loader').fadeOut(); }, 1000);
+
+
+
+var wow = new WOW(
+  {
+    boxClass:     'wow',      
+    animateClass: 'animated',
+    offset:       0,          
+    mobile:       true,       
+    live:         true,       
+    callback:     function(box) {
+     
+    },
+    scrollContainer: null,  
+    resetAnimation: true,  
+  }
+);
+wow.init();
+
+
+// scroll-Top
+
+$(window).scroll(function(){
+  if($(this).scrollTop() > 500){
+    $('.scrolltotop').fadeIn();
+  }
+  else{
+    $('.scrolltotop').fadeOut();
+  }
+   
+   
+});
+   
+$('.scrolltotop').click(function(){
+  $('html,body').animate({scrollTop:0}, 1000);
+  return false;
+});
+
+
+
+
+});
+
